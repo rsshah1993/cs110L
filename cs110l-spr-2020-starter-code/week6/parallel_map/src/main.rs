3e883ee8deedc87e4d375dd84b5afec9695920d2
Romil Shah <roshah@umich.edu>
2021-03-23 21:56:00 -0400

    got multi-channel working -- needs sorting

--- cs110l-spr-2020-starter-code/week6/parallel_map/src/main.rs
+++ cs110l-spr-2020-starter-code/week6/parallel_map/src/main.rs
@@ -1,18 +1,50 @@
 use crossbeam_channel;
 use std::{thread, time};
+use std::sync::{Arc, Mutex};
+
 
 fn parallel_map<T, U, F>(mut input_vec: Vec<T>, num_threads: usize, f: F) -> Vec<U>
 where
     F: FnOnce(T) -> U + Send + Copy + 'static,
-    T: Send + 'static,
-    U: Send + 'static + Default,
+    T: Send + 'static +,
+    U: Send + 'static + Default + std::clone::Clone,
 {
     let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
+    let mut rc_output_vec = Arc::new(Mutex::new(output_vec));
+
     // TODO: implement parallel map!
-    output_vec
+    let (sender, receiver) = crossbeam_channel::unbounded();
+    let mut threads = Vec::new();
+    for _ in 0..num_threads {
+        let receiver = receiver.clone();
+        let rc_output_vec_cloned = rc_output_vec.clone();
+        threads.push(thread::spawn(move || {
+            while let Ok(input_num) = receiver.recv() {
+                // let (input_num, index) = sent_value;
+                let output = f(input_num);
+                rc_output_vec_cloned.lock().unwrap().push(output);
+            }
+        }))
+    }
+
+    loop{
+        let input_num = input_vec.pop();
+        match input_num {
+            Some(input_num) => sender.send(input_num).expect("No receivers!"),
+            None => break
+
+        }        
+    }
+
+    drop(sender);
+    for thread in threads {
+        thread.join().expect("Thread panicked!");
+    }
+    let x = rc_output_vec.lock().unwrap().to_vec(); x
 }
 
 fn main() {
+    let num_threads = num_cpus::get();
     let v = vec![6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 12, 18, 11, 5, 20];
     let squares = parallel_map(v, 10, |num| {
         println!("{} squared is {}", num, num * num);
