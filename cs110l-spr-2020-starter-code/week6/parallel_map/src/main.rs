a3ef80a16908727fcaeaf8e0f64225563530b0de
Romil Shah <roshah@umich.edu>
2021-03-27 15:07:03 -0400

    got multiple channels working, TODO: ordering according to input vector

--- cs110l-spr-2020-starter-code/week6/parallel_map/src/main.rs
+++ cs110l-spr-2020-starter-code/week6/parallel_map/src/main.rs
@@ -1,50 +1,51 @@
 use crossbeam_channel;
 use std::{thread, time};
-use std::sync::{Arc, Mutex};
+
 
 
 fn parallel_map<T, U, F>(mut input_vec: Vec<T>, num_threads: usize, f: F) -> Vec<U>
 where
-    F: FnOnce(T) -> U + Send + Copy + 'static,
-    T: Send + 'static +,
-    U: Send + 'static + Default + std::clone::Clone,
-{
-    let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
-    let mut rc_output_vec = Arc::new(Mutex::new(output_vec));
-
-    // TODO: implement parallel map!
+    F: FnOnce(T) -> U + Send + Copy + 'static + std::marker::Sync,
+    T: Send + 'static,
+    U: Send + 'static + Default + std::clone::Clone + std::fmt::Debug,
+{   
+    let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());    
+   
+    // input channel
     let (sender, receiver) = crossbeam_channel::unbounded();
+    // output channel
+    let (sender2, receiver2) = crossbeam_channel::bounded(input_vec.len());
     let mut threads = Vec::new();
     for _ in 0..num_threads {
         let receiver = receiver.clone();
-        let rc_output_vec_cloned = rc_output_vec.clone();
+        let sender2 = sender2.clone();
         threads.push(thread::spawn(move || {
-            while let Ok(input_num) = receiver.recv() {
-                // let (input_num, index) = sent_value;
-                let output = f(input_num);
-                rc_output_vec_cloned.lock().unwrap().push(output);
-            }
-        }))
+                while let Ok((index, input_num)) = receiver.recv() {
+                    let output_num = f(input_num);
+                    let _ = sender2.send((index, output_num));
+                }
+            }))
+        }
+    
+    let mut index = input_vec.len();
+    while let Some(input_num) = input_vec.pop() {
+        let _ = sender.send((index, input_num));
+        index -= 1
     }
-
-    loop{
-        let input_num = input_vec.pop();
-        match input_num {
-            Some(input_num) => sender.send(input_num).expect("No receivers!"),
-            None => break
-
-        }        
-    }
-
+    // drop both senders to close channels
     drop(sender);
-    for thread in threads {
-        thread.join().expect("Thread panicked!");
+    drop(sender2);
+
+    // iterate through output channel
+    // TODO: need to sort this, have the index where it should belong
+    // but how to do this without sorting the output vector?
+    for (_, output_val) in receiver2.iter() {
+        output_vec.push(output_val)
     }
-    let x = rc_output_vec.lock().unwrap().to_vec(); x
+    output_vec
 }
 
 fn main() {
-    let num_threads = num_cpus::get();
     let v = vec![6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 12, 18, 11, 5, 20];
     let squares = parallel_map(v, 10, |num| {
         println!("{} squared is {}", num, num * num);
